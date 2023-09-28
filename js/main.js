091ae9d73db943fb26736d0d4f94d6759d375526
l.engelhardt <l.engelhardt@edu.letteverein.berlin>
2023-09-28 15:05:39 +0200

    js table fix and button color

--- js/main.js
+++ js/main.js
@@ -1,35 +1,60 @@
+let tableGenerated = false;
+
 function initializeBoard() {
-  const tbl = document.createElement("table");
-  const tblBody = document.createElement("tbody");
+    if (tableGenerated) return;
+    tableGenerated = true;
 
-  for (let i = 1; i < 26; i++) {
-    const row = document.createElement("tr");
+    const tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody");
 
-    for (let j = 1; j < 26; j++) {
-      const cell = document.createElement("td");
-      cell.style.width = "2em";
-      cell.style.height = "2em";
-      cell.style.border = "1px solid white";
-      
-      //const cellText = document.createTextNode(`${i}, ${j}`);
-      //cell.appendChild(cellText);
-      
-      row.appendChild(cell);
+    for (let i = 1; i <= 25; i++) {
+        const row = document.createElement("tr");
+
+        for (let j = 1; j <= 25; j++) {
+            const cell = document.createElement("td");
+
+            cell.setAttribute("ondragover", "allowDrop(event)");
+            cell.setAttribute("ondrop", "drop(event)");
+
+            cell.style.width = "2em";
+            cell.style.height = "2em";
+            cell.style.border = "1px solid white";
+
+            //const cellText = document.createTextNode(`${i}, ${j}`);
+            //cell.appendChild(cellText);
+
+            row.appendChild(cell);
+        }
+
+        tblBody.appendChild(row);
     }
 
-    tblBody.appendChild(row);
-  }
-  
-  tblBody.setAttribute("border", "2");
-  tbl.appendChild(tblBody);
-  const tableDiv = document.getElementById("table");
-  tableDiv.appendChild(tbl);
-  tbl.setAttribute("border", "2");
+    tbl.appendChild(tblBody);
+
+    const tableContainer = document.getElementById("table");
+    tableContainer.appendChild(tbl);
 }
 
-
-
-
 function resetBoard() {
+    const tableContainer = document.getElementById("table");
+    tableContainer.innerHTML = "";
 
+    tableGenerated = false;
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault();
+    const data = event.dataTransfer.getData("text");
+    const boatSize = parseInt(data);
+    const cell = event.target;
+
+    if (!cell.hasAttribute("data-occupied")) {
+        cell.setAttribute("data-occupied", "true");
+        cell.style.backgroundColor = "blue";
+        cell.textContent = boatSize;
+    }
 }
