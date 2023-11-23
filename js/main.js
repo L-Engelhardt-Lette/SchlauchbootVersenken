20c99b9994ba570c563ebb33655b4002f49723f2
jacobheine1 <j.heine@edu.letteverein.berlin>
2023-11-23 14:07:41 +0100

    schusstest7.0

--- js/main.js
+++ js/main.js
@@ -21,6 +21,8 @@ function initializeBoard() {
             cell.style.width = "2em";
             cell.style.height = "2em";
             cell.style.border = "1px solid white";
+            cell.setAttribute("data-row", i);
+            cell.setAttribute("data-col", j);
 
             row.appendChild(cell);
         }
@@ -82,62 +84,53 @@ function drop(event) {
 
         const img = document.getElementById(data);
 
-        /* Create an image element and set its source based on the team color
-        const img = document.createElement("img");
-        img.src = `/img/ships_new/ship-${boatSize}-${team}.png`;
-        */
-        
         // Set the size of the image to match the cell size
         img.style.width = "100%";
         img.style.height = "100%";
-        
+
         // Append the image to the cell
         cell.appendChild(img);
+
+        // Set the ship's position data attributes
+        cell.setAttribute("data-ship-row", cell.getAttribute("data-row"));
+        cell.setAttribute("data-ship-col", cell.getAttribute("data-col"));
     }
 
     // Hide the dragged image
     const draggedElement = document.getElementById(boatSize.toString());
     draggedElement.style.display = "none";
 }
-
-// Gleichungen
-let m = 0;
-
-let b = 0;
-
-let schuss = function f(x){
-    return m*x+0;
-} 
-let schiff = function g(x){
-    return 0*x+b;
-}
-
-function showShot(x){
-    const y = f(x);
-}
-function checkIntersection() {
-    const xIntersection = (b-0)/(m-0);
-    const yIntersection1 = schuss(xIntersection);
-    const yIntersection2 = schiff(xIntersection);
-
-    console.log(`Die Funktionen schneiden sich bei (${xIntersection}, ${yIntersection1}) und (${xIntersection}, ${yIntersection2}).`);
-}
-function fireShot(){
+// Diese Funktion wird aufgerufen, wenn der Schussbutton gedrückt wird
+function onSchussButtonClicked() {
     const schussFormelInput = document.getElementById("schussFormel");
     const schussFormel = schussFormelInput.value;
+
+    // Beispiel: Schuss auf das Spielfeld mit der eingegebenen Formel
+    fireShot(schussFormel);
+}
+
+// Füge einen Event Listener zum Schussbutton hinzu
+const schussButton = document.getElementById("schussButton");
+schussButton.addEventListener("click", onSchussButtonClicked);
+function fireShot(formula) {
+    const schussFormelInput = document.getElementById("schussFormel");
+    const schussFormel = formula || schussFormelInput.value;
+
     try {
         const parsedFormel = new Function('x', `return ${schussFormel}`);
-        const x = 3; // Beispielwert für x, du kannst dies anpassen
-        const y = parsedFormel(x);
-
-        // Hier fügst du den Code ein, um den Schuss auf dem Spielfeld anzuzeigen
-        console.log(`Schuss abgefeuert auf Position (${x}, ${y})`);
-
-        // Vergleiche die Gleichungen
-        if (schuss.toString() === schiff.toString()) {
-            console.log("Die Gleichungen sind gleich.");
-        } else {
-            console.log("Die Gleichungen sind nicht gleich.");
+        
+        // Iterate through each cell to check for hits
+        for (let i = 1; i <= 10; i++) {
+            for (let j = 1; j <= 10; j++) {
+                const x = j;
+                const y = parsedFormel(x);
+                
+                // Check if the shot hits a ship
+                if (checkHit(x, y)) {
+                    console.log(`Hit at position (${x}, ${y})!`);
+                    // Additional logic for a hit, e.g., marking the cell as hit
+                }
+            }
         }
 
         schussFormelInput.value = ""; // Zurücksetzen der Eingabe
@@ -148,4 +141,20 @@ function fireShot(){
     }
 }
 
+function checkHit(x, y) {
+    // Check each ship for a hit
+    const ships = document.querySelectorAll(".draggable-boat");
+    for (const ship of ships) {
+        const shipRow = parseInt(ship.getAttribute("data-ship-row"));
+        const shipCol = parseInt(ship.getAttribute("data-ship-col"));
 
+        if (x === shipCol && y === shipRow) {
+            return true; // Hit!
+        }
+    }
+
+    return false; // No hit
+}
+
+// Beispiel: Schuss auf das Spielfeld mit der Formel "2*x+1"
+fireShot("2*x+1");
